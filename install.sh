9ae24a6d13eb353e000393f1af0be9811f233333
Mikołaj Badyl <mikolajbadyl@mailbox.org>
2022-03-29 09:45:46 +0200

    install script updates

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 wget https://github.com/hawier-dev/appimages-dl/releases/download/v0.0.1/appimgdl
 chmod +x appimgdl
-mv appimgdl ~/.local/bin/
+echo "Moving executable to /usr/share/bin/"
+sudo mv appimgdl /usr/share/bin/
 echo "Installed"
