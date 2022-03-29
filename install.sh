a17708a731618b30a59f9d2d6b02531928c18097
Mikołaj Badyl <mikolajbadyl@mailbox.org>
2022-03-29 09:51:53 +0200

    changed path

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 wget https://github.com/hawier-dev/appimages-dl/releases/download/v0.0.1/appimgdl
 chmod +x appimgdl
 echo "Moving executable to /usr/share/bin/"
-sudo mkdir -p /usr/share/bin/
-sudo mv appimgdl /usr/share/bin/
+sudo mkdir -p /usr/local/bin/
+sudo mv appimgdl /usr/local/bin/
 echo "Installed"
