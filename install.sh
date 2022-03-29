e882cc124667d86cc275d8330f579eaf4adfa933
Mikołaj Badyl <mikolajbadyl@mailbox.org>
2022-03-29 10:24:33 +0200

    pip installation

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+curl https://bootstrap.pypa.io/get-pip.py | python
 python -m pip install -r https://raw.githubusercontent.com/hawier-dev/appimages-dl/main/requirements.txt
 wget https://github.com/hawier-dev/appimages-dl/releases/download/v0.0.1/appimgdl
 chmod +x appimgdl
